7fc590c5ebec79340268f90c3203fe7c10b366a1
sandipsaha597 <40121135+sandipsaha597@users.noreply.github.com>
2020-04-19 18:27:54 +0530

    Add files via upload

---
+++ precache-manifest.70764d045b25f1414e6041f91e567eb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce192ce3b2dc81c7cc956cc65b2463d8",
+    "url": "/multi-page-website/index.html"
+  },
+  {
+    "revision": "aa57b4666d0d3894f9a9",
+    "url": "/multi-page-website/static/css/main.5e0fd931.chunk.css"
+  },
+  {
+    "revision": "ae37f2f4838b4082ec83",
+    "url": "/multi-page-website/static/js/2.86cd8ab0.chunk.js"
+  },
+  {
+    "revision": "723894b771c1fe011b0df55ec646ea2d",
+    "url": "/multi-page-website/static/js/2.86cd8ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa57b4666d0d3894f9a9",
+    "url": "/multi-page-website/static/js/main.45324bf5.chunk.js"
+  },
+  {
+    "revision": "7e67cb55cf74a388bec3",
+    "url": "/multi-page-website/static/js/runtime-main.5c68fb5b.js"
+  }
+]);
